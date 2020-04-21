22163fbd861bfbf3335cdd82954e3440d7997de8
Anmol Dadlani <anmoldadlani@gmail.com>
2020-04-22 00:19:38 +0100

    name updates when changed

--- app/src/main/java/and/coursework/fitnesse/ProfileActivity.java
+++ app/src/main/java/and/coursework/fitnesse/ProfileActivity.java
@@ -1,10 +1,6 @@
 package and.coursework.fitnesse;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +10,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,29 +20,35 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class ProfileActivity extends AppCompatActivity {
+    Button signOut;
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
+    ProgressBar progressBar;
+    TextView email;
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        TextView email = findViewById(R.id.Email);
-        TextView name = findViewById(R.id.Name);
+        email = findViewById(R.id.Email);
+        name = findViewById(R.id.Name);
 
-        Button signOut = findViewById(R.id.signoutButton);
+        signOut = findViewById(R.id.signoutButton);
         Button saveChanges = findViewById(R.id.saveChangesButton);
 
         CheckBox verified = findViewById(R.id.verifiedBox);
 
-        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar = findViewById(R.id.progressBar);
 
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        final FirebaseUser mUser = mAuth.getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
 
         progressBar.setVisibility(View.INVISIBLE);
 
         assert mUser != null;
-        Log.e("This is User: ", String.valueOf(mUser.getDisplayName()));
+        Log.d("This is User: ", String.valueOf(mUser.getDisplayName()));
 
 
         email.setText(mUser.getEmail());
@@ -57,8 +62,7 @@ public class ProfileActivity extends AppCompatActivity {
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                signOutClicked();
             }
         });
 
@@ -66,25 +70,38 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 progressBar.setVisibility(View.VISIBLE);
-                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                        .setDisplayName("Anmol69")
-                        .build();
-
-                mUser.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()){
-                            Toast.makeText(ProfileActivity.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.INVISIBLE);
-
-                        }
-                    }
-                });
-
+                String newName = getNewName();
+                updateProfile(newName);
             }
         });
 
 
 
+    }
+
+    private String getNewName() {
+        return name.getText().toString();
+    }
+
+    void signOutClicked(){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+    }
+
+    void updateProfile(String newName){
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(newName)
+                .build();
+
+        mUser.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(ProfileActivity.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+
+                }
+            }
+        });
     }
 }
