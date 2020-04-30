f7322c95bcd46700f67400c7be9ba019da8f62c1
Anmol Dadlani <anmoldadlani@gmail.com>
2020-04-30 02:25:06 +0100

    gets searched email

--- app/src/main/java/and/coursework/fitnesse/AddFriendActivity.java
+++ app/src/main/java/and/coursework/fitnesse/AddFriendActivity.java
@@ -6,13 +6,44 @@ import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class AddFriendActivity extends AppCompatActivity {
+
+    Button searchButton;
+    EditText emailSearch;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        emailSearch = findViewById(R.id.emailSearch);
+
+        searchButton = findViewById(R.id.searchFriendsButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Query query = mDatabase.orderByChild("Email").equalTo(emailSearch.getText().toString().trim());
+            }
+        });
     }
 
     @Override
