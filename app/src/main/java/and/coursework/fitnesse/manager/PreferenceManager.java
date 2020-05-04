f46e308894191c7c780dfb02baa5aab2fe5a5788
Anmol Dadlani <anmoldadlani@gmail.com>
2020-05-04 02:41:22 +0100

    preference manager for saving notifications

--- app/src/main/java/and/coursework/fitnesse/manager/PreferenceManager.java
+++ app/src/main/java/and/coursework/fitnesse/manager/PreferenceManager.java
@@ -1,4 +1,21 @@
 package and.coursework.fitnesse.manager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class PreferenceManager {
+
+    Context context;
+
+    public PreferenceManager(Context context) {
+        this.context = context;
+    }
+
+    private void saveNotificationPreference(boolean notificationsPreference){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("areNotificationsEnabled", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("Notifications", notificationsPreference);
+        editor.apply();
+    }
+
 }
