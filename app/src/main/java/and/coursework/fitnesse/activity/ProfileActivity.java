13b64c163dbd6d2694794839c55b45229686b095
Anmol Dadlani <anmoldadlani@gmail.com>
2020-05-06 03:20:10 +0100

    added swipe gestures

--- app/src/main/java/and/coursework/fitnesse/activity/ProfileActivity.java
+++ app/src/main/java/and/coursework/fitnesse/activity/ProfileActivity.java
@@ -1,10 +1,8 @@
 package and.coursework.fitnesse.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -12,6 +10,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -25,13 +24,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import and.coursework.fitnesse.R;
+import and.coursework.fitnesse.listeners.OnSwipeTouchListener;
 import and.coursework.fitnesse.manager.PreferenceManager;
 
-public class ProfileActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
-    private static final int swipeThreshold = 100;
-    private static final int swipeVelocityThreshold = 100;
-
-    GestureDetector gestureDetectorProfile = new GestureDetector(this);
+public class ProfileActivity extends AppCompatActivity{
 
     Button signOut;
     FirebaseAuth mAuth;
@@ -42,6 +38,7 @@ public class ProfileActivity extends AppCompatActivity implements GestureDetecto
     CheckBox notificationsCheckBox;
     TimePicker timePicker;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -58,6 +55,7 @@ public class ProfileActivity extends AppCompatActivity implements GestureDetecto
         Button saveChangesButton = findViewById(R.id.saveChangesButton);
         ImageView verified = findViewById(R.id.verifiedBox);
         ImageView aboutImageView = findViewById(R.id.about);
+        RelativeLayout relativeLayout = findViewById(R.id.profileRelativeLayout);
 
         timePicker.setIs24HourView(true);
         progressBar.setVisibility(View.INVISIBLE);
@@ -96,6 +94,22 @@ public class ProfileActivity extends AppCompatActivity implements GestureDetecto
                 Toast.makeText(getApplicationContext(), "Please set the time you would like to " +
                         "be reminded to complete your daily activity", Toast.LENGTH_SHORT).show();
                 setSavedNotificationTimeToTimePicker();
+            }
+        });
+
+        relativeLayout.setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()){
+            public void onSwipeTop() {
+                saveChanges();
+            }
+            public void onSwipeRight() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(100, R.anim.fade_in);
+            }
+            public void onSwipeLeft() {
+                startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+            }
+            public void onSwipeBottom() {
+                signOutClicked();
             }
         });
     }
@@ -137,83 +151,6 @@ public class ProfileActivity extends AppCompatActivity implements GestureDetecto
 
         saveNotificationPreference(notificationsCheckBox.isChecked());
         getSavedNotificationTime();
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent downEvent, MotionEvent moveEvent, float velocityX, float velocityY) {
-        Log.d("Anmol", "onFling: called");
-
-        boolean result = false;
-        float diffY = moveEvent.getY() - downEvent.getY();
-        float diffX = moveEvent.getX() - downEvent.getX();
-
-        if (Math.abs(diffX) > Math.abs(diffY)){
-            // right or left swipe
-            if (Math.abs(diffX) > swipeThreshold && Math.abs(velocityX) > swipeVelocityThreshold){
-                if (diffX > 0)
-                    onSwipeRight();
-                else
-                    onSwipeLeft();
-                result = true;
-            }
-        } else{
-            // up or down swipe
-            if (Math.abs(diffY) > swipeThreshold && Math.abs(velocityY) > swipeVelocityThreshold){
-                if (diffY > 0)
-                    onSwipeBottom();
-                else
-                    onSwipeUp();
-                result = true;
-            }
-        }
-
-
-        return result;
-    }
-
-    private void onSwipeUp() {
-        saveChanges();
-    }
-
-    private void onSwipeBottom() {
-    }
-
-    private void onSwipeLeft() {
-    }
-
-    private void onSwipeRight() {
-        saveChanges();
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        gestureDetectorProfile.onTouchEvent(event);
-        return super.onTouchEvent(event);
     }
 
     private void saveNotificationPreference(boolean enabled) {
