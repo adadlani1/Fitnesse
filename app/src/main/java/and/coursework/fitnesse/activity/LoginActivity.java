3bde2fccf356f42b39fb3086e6471cc0acf6f803
Anmol Dadlani <anmoldadlani@gmail.com>
2020-05-09 03:48:40 +0100

    cleaned up and commented login activity

--- app/src/main/java/and/coursework/fitnesse/activity/LoginActivity.java
+++ app/src/main/java/and/coursework/fitnesse/activity/LoginActivity.java
@@ -1,8 +1,5 @@
 package and.coursework.fitnesse.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,9 +11,8 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -41,17 +37,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mAuth = FirebaseAuth.getInstance();
+        initialiseVariables();
 
-        emailField = findViewById(R.id.emailText);
-        passwordField = findViewById(R.id.password);
-
-        loginButton = findViewById(R.id.loginButton);
-        signUpButton = findViewById(R.id.signUpButton);
-
-        progressBar = findViewById(R.id.progressBar);
-
-        progressBar.setVisibility(View.INVISIBLE);
+        /*when loginButton is pressed*/
         loginButton.setOnClickListener(v -> {
             String email = emailField.getText().toString().trim();
             String password = passwordField.getText().toString().trim();
@@ -63,33 +51,40 @@ public class LoginActivity extends AppCompatActivity {
             if (TextUtils.isEmpty(password))
                 passwordField.setError("Password is Required");
 
-
             if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
-                progressBar.setVisibility(View.VISIBLE);
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                            progressBar.setVisibility(View.INVISIBLE);
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Error! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar.setVisibility(View.INVISIBLE);
-                        }
-
-                    }
-                });
+                signIn(email, password);
             }
         });
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
-            }
-        });
+        /*when sign up button is pressed*/
+        signUpButton.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
 
+    }
+
+    private void initialiseVariables() {
+        mAuth = FirebaseAuth.getInstance();
+        emailField = findViewById(R.id.emailText);
+        passwordField = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    /*starts sign in sequence*/
+    private void signIn(String email, String password) {
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                progressBar.setVisibility(View.INVISIBLE);
+            } else {
+                Toast.makeText(LoginActivity.this, "Error! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+        });
     }
 
     @Override
