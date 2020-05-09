1614fa326afb65202a29635746bbf2b300319360
Anmol Dadlani <anmoldadlani@gmail.com>
2020-05-09 04:28:54 +0100

    cleaned and commented register

--- app/src/main/java/and/coursework/fitnesse/activity/RegisterActivity.java
+++ app/src/main/java/and/coursework/fitnesse/activity/RegisterActivity.java
@@ -10,12 +10,9 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -31,8 +28,8 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     private EditText emailField, passwordField, nameField;
-
     private ProgressBar progressBar;
+    private Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        emailField = findViewById(R.id.emailText);
-        passwordField = findViewById(R.id.password);
-        nameField = findViewById(R.id.nameText);
-
-        Button signUpButton = findViewById(R.id.signUpButton);
-
-        progressBar = findViewById(R.id.progressBar2);
-
-        progressBar.setVisibility(View.INVISIBLE);
+        initialiseViews();
 
         signUpButton.setOnClickListener(v -> {
 
@@ -59,6 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
             String password = passwordField.getText().toString().trim();
             String name = nameField.getText().toString();
 
+            /*Statements for validation*/
             if (TextUtils.isEmpty(email)) {
                 emailField.setError("Email is Required");
             }
@@ -67,24 +57,39 @@ public class RegisterActivity extends AppCompatActivity {
                 passwordField.setError("Password is Required");
 
             if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(name)) {
-                progressBar.setVisibility(View.VISIBLE);
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        createNewUser(Objects.requireNonNull(Objects.requireNonNull(task.getResult()).getUser()));
-                        Toast.makeText(RegisterActivity.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
-                        onBackPressed();
-                    } else {
-                        Toast.makeText(RegisterActivity.this, "Error! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                        progressBar.setVisibility(View.INVISIBLE);
-                    }
-
-                });
+                registerSequence(email, password);
             }
         });
 
 
     }
 
+    /*Registers user using values inputted*/
+    private void registerSequence(String email, String password) {
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                createNewUser(Objects.requireNonNull(Objects.requireNonNull(task.getResult()).getUser()));
+                Toast.makeText(RegisterActivity.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                onBackPressed();
+            } else {
+                Toast.makeText(RegisterActivity.this, "Error! " + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+        });
+    }
+
+    private void initialiseViews() {
+        emailField = findViewById(R.id.emailText);
+        passwordField = findViewById(R.id.password);
+        nameField = findViewById(R.id.nameText);
+        signUpButton = findViewById(R.id.signUpButton);
+        progressBar = findViewById(R.id.progressBar2);
+        progressBar.setVisibility(View.INVISIBLE);
+    }
+
+    /*Adds name and email to realtime database*/
     private void createNewUser(FirebaseUser newUser) {
 
         String email = newUser.getEmail();
@@ -96,9 +101,10 @@ public class RegisterActivity extends AppCompatActivity {
                 .build();
         newUser.updateProfile(profileUpdates).addOnCompleteListener(Task::isSuccessful);
 
+        /*Sends verficartion email to user's email*/
         newUser.sendEmailVerification();
 
-
+        /*Adds details to database*/
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userID).child("Email");
         mDatabase.setValue(email);
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userID).child("Name");
