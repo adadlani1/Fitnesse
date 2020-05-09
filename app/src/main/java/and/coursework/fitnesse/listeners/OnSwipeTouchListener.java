8243527262566d93a866f2b4259603aa2e7c3445
Anmol Dadlani <anmoldadlani@gmail.com>
2020-05-09 20:40:22 +0100

    cleandd and commented listener

--- app/src/main/java/and/coursework/fitnesse/listeners/OnSwipeTouchListener.java
+++ app/src/main/java/and/coursework/fitnesse/listeners/OnSwipeTouchListener.java
@@ -1,17 +1,21 @@
 package and.coursework.fitnesse.listeners;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
+/*Touch Listener that checks for gestures and depending on movement of the finger, the swipe
+* loads a specific method*/
 public class OnSwipeTouchListener implements View.OnTouchListener {
     private final GestureDetector gestureDetector;
 
-    public OnSwipeTouchListener (Context ctx){
+    protected OnSwipeTouchListener(Context ctx){
         gestureDetector = new GestureDetector(ctx, new GestureListener());
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         return gestureDetector.onTouchEvent(event);
@@ -19,7 +23,8 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
 
     private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
 
-        private static final int SWIPE_THRESHOLD = 100;
+        /*Constants for speed and length of swipe*/
+        private static final int SWIPE_THRESHOLD = 200;
         private static final int SWIPE_VELOCITY_THRESHOLD = 100;
 
         @Override
@@ -28,12 +33,15 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
         }
 
         @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        public boolean onFling(MotionEvent motionEvent1, MotionEvent motionEvent2, float velocityX, float velocityY) {
             boolean result = false;
             try {
-                float diffY = e2.getY() - e1.getY();
-                float diffX = e2.getX() - e1.getX();
+                /*Finds difference between the start point and end point*/
+                float diffY = motionEvent2.getY() - motionEvent1.getY();
+                float diffX = motionEvent2.getX() - motionEvent1.getX();
+                /*Finds if the swipe is vertical or horizontal*/
                 if (Math.abs(diffX) > Math.abs(diffY)) {
+                    /*Checks if the registered swipe is fast and longer than the constants*/
                     if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
                         if (diffX > 0) {
                             onSwipeRight();
